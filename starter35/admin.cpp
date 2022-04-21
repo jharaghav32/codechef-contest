3f20630b32e9182a31422c856d00eba2e44d83d8
jharaghav <raghavkr1000@gmail.com>
2022-04-21 16:20:14 +0530

    1st q

---
+++ starter35/admin.cpp
@@ -0,0 +1,47 @@
+// problem link:https://www.codechef.com/START35D/problems/ADMINSHOP
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define pb push_back
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+const int Mod = 1'000'000'007;
+const int N = 3e5, M = N;
+ll lcm(int a,int b){
+    ll a1=a;
+    ll b1=b;
+    return a1*b1/__gcd(a1,b1);
+}
+void solved(){
+    int n,x;
+    cin>>n>>x;
+    int arr[n];
+    int mini = INT_MAX;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+        mini = min(mini,arr[i]);
+    }
+    int ans;
+    if(n>=x) ans = n;
+    else{
+        if(x%mini==0) ans = max(n,x/mini);
+        else
+        ans = max(n,(x/mini)+1);
+    
+    }
+    cout<<ans<<endl;
+}
+int main(){
+ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+    int t;
+    cin>>t;
+    while(t--){
+        solved();
+    }
+return 0;
+}
