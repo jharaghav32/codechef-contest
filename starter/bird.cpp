34a41954d170066d63a8af11fec9d2209c2fb722
jharaghav <jharaghav3209@gmail.com>
2022-04-28 22:12:41 +0530

    starter 36

---
+++ starter/bird.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define pb push_back
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+const int Mod = 1'000'000'007;
+const int N = 3e5, M = N;
+ll lcm(int a,int b){
+    ll a1=a;
+    ll b1=b;
+    return a1*b1/__gcd(a1,b1);
+}
+void solved(){
+    int x,y,z;
+    cin>>x>>y>>z;
+    if(z%x==0 && z%y==0)
+    cout<<"ANY"<<endl;
+    else if(z%x==0)
+    cout<<"CHICKEN"<<endl;
+    else if(z%y==0)
+    cout<<"DUCK"<<endl;
+    else
+    cout<<"NONE"<<endl;
+}
+int main(){
+ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+    int t;
+    cin>>t;
+    while(t--){
+        solved();
+    }
+return 0;
+}
