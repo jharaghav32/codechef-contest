7eafc9b9db44f191e0a9f421ef13b43adea2528d
jharaghav <jharaghav3209@gmail.com>
2022-05-02 07:05:48 +0530

    another approach

--- practice/calculator.cpp
+++ practice/calculator.cpp
@@ -16,19 +16,35 @@ ll lcm(int a,int b){
     return a1*b1/__gcd(a1,b1);
 }
 void solved(){
-   ll n,b;
+//    ll n,b;
+//    cin>>n>>b;
+//    ll ans =0;
+//    ll val;
+//    ll nmoves=n/b;
+//    ll first;
+//    while(nmoves){
+//     first=(n-nmoves*b);
+//     val = nmoves*first;
+//     ans = max(val,ans);
+//     nmoves--;
+// another approach
+ll n,b;
    cin>>n>>b;
-   ll ans =0;
-   ll val;
-   ll nmoves=n/b;
-   ll first;
-   while(nmoves){
-    first=(n-nmoves*b);
-    val = nmoves*first;
-    ans = max(val,ans);
-    nmoves--;
-   } 
+    ll nmoves=((n/(2*b)));
+    ll nmoves1= nmoves+1;
+   if(nmoves>n){
+       nmoves=0;
+   }
+   if(nmoves1>n)
+   nmoves1=0;
+   ll ans;
+  
+   ans = max(nmoves1*(n-nmoves1*b), nmoves*(n-nmoves*b));
+   
+   
+   
    cout<<ans<<"\n";
+   } 
 }
 int main(){
 ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
