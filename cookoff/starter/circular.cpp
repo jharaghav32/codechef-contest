4fe958b563839933f8e743449d053743cae7c8eb
jharaghav <jharaghav3209@gmail.com>
2022-05-11 22:55:39 +0530

    starter 11 may

---
+++ cookoff/starter/circular.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define pb push_back
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+const int Mod = 1'000'000'007;
+const int N = 3e5, M = N;
+ll lcm(int a,int b){
+    ll a1=a;
+    ll b1=b;
+    return a1*b1/__gcd(a1,b1);
+}
+void solved(){
+    ll a,b,m;
+    cin>>a>>b>>m;
+    ll ans1=abs(b-a);
+    ll ans2=abs(m-ans1);
+    if(ans1>=ans2)
+    cout<<ans2<<"\n";
+    else
+    cout<<ans1<<"\n";
+}
+int main(){
+ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+    int t;
+    cin>>t;
+    while(t--){
+        solved();
+    }
+return 0;
+}
