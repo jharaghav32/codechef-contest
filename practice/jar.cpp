08f225f002baab25104de34f63e9fe2d4793cef9
jharaghav <jharaghav3209@gmail.com>
2022-05-15 09:51:17 +0530

    practice 15 may

---
+++ practice/jar.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define pb push_back
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+const int Mod = 1'000'000'007;
+const int N = 3e5, M = N;
+ll lcm(int a,int b){
+    ll a1=a;
+    ll b1=b;
+    return a1*b1/__gcd(a1,b1);
+}
+void solved(){
+    int n;
+    cin>>n;
+    int arr[n];
+    ll sum=0;
+    int maxi=INT_MIN;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+        sum += arr[i]-1;
+    }
+    cout<<sum+1<<endl;
+}
+int main(){
+ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+    int t;
+    cin>>t;
+    while(t--){
+        solved();
+    }
+return 0;
+}
