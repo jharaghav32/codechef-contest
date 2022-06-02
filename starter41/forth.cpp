d39beb9e3ce978804e0fb2cb1bba04611957fa58
jharaghav <jharaghav3209@gmail.com>
2022-06-02 06:59:16 +0530

    starter 41

---
+++ starter41/forth.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define pb push_back
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+const int Mod = 1'000'000'007;
+const int N = 3e5, M = N;
+ll lcm(int a,int b){
+    ll a1=a;
+    ll b1=b;
+    return a1*b1/__gcd(a1,b1);
+}
+void solved(){
+    ll x,y;
+    cin>>x>>y;
+    ll ans;
+    if(x<=y){
+        ans=(y-x);
+    }
+    else{
+        if((x-y)%2==0)
+        ans=(x-y)/2;
+        else{
+            int val = ((x+1)-y)/2;
+            ans = val+1;
+        }
+    }
+    cout<<ans<<"\n";
+}
+int main(){
+ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+    int t;
+    cin>>t;
+    while(t--){
+        solved();
+    }
+return 0;
+}
